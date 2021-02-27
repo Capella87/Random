b462b44a83cb96d34b7ad1e8a54e34a13c38716f
Capella87 <contact@capelladev.com>
2021-02-27 17:41:43 +0900

    Separate design function to new file and rename to intro

---
+++ Random/cui.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include "cui.h"
+
+void intro(void)
+{
+    puts("Random - Select numbers in randomized ways");
+    printf("Developed by Capella87.\n");
+    printf("%s | %s\n", VERSION, RELEASE_DATE);
+    printf("Copyright (c) 2018-%d Capella87\n", YEAR);
+    puts("This software is distributed under MIT License. See LICENCE at the root directory for more details.");
+    puts("https://github.com/Capella87/Random");
+    puts("---------------------------------------------\n");
+}
---
+++ Random/cui.h
@@ -0,0 +1,21 @@
+/****************************************************************************
+ *    Copyright (c) 2021 Capella87                                          *
+ *                                                                          *
+ *   This file is part of Random program.                                   *
+ *   github.com/Capella87/Random                                            *
+ ****************************************************************************/
+
+// Compromise functions and constants for the User interface in CLI mode.
+
+#ifndef __CUI_H__
+#define __CUI_H__
+#include <stdbool.h>
+
+#define WAIT_DEFAULT    1
+#define YEAR            2021
+#define VERSION         "1.1.1"
+#define RELEASE_DATE    "Feb 17, 2021"
+
+void intro(void);
+
+#endif
