f69738cf2c37b50b6c602db5deead73fd5a8104c
Capella87 <contact@capelladev.com>
2021-10-17 20:08:07 +0900

    Implement xorshift64

---
+++ Random/xor.h
@@ -0,0 +1,25 @@
+#ifndef XOR_H
+#define XOR_H
+#include "core.h"
+#include <stdbool.h>
+typedef long long int64;
+typedef unsigned long long uint64;
+typedef enum _sortOptions { sorted, unsorted } SortOpt;
+
+// ref: https://github.com/Bunny83/Utilities/blob/master/XorShift64.cs
+
+const uint64 XOR64_OFFSET = 13726359678912485784UL;
+typedef struct _xor64
+{
+    uint64 seed;
+    uint64* result;
+    int count;
+} Xor64;
+
+static void initXor64(Xor64* t);
+uint64 getXor64(Xor64* t);
+uint64 getXor64Num(Xor64* t, uint64 min, uint64 max);
+bool randXor64(uint64 min, uint64 max, const int count, SortOpt op);
+bool resetXor64Seed(Xor64* t);
+
+#endif
---
+++ Random/xor64.c
@@ -0,0 +1,46 @@
+#include "xor.h"
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+#include "core.h"
+
+static void initXor64(Xor64* t)
+{
+    t->seed = (uint64)time(0) ^ XOR64_OFFSET;
+    getXor64(t);
+    t->seed ^= (uint64)time(0) << 13;
+    t->seed ^= (uint64)time(0) << 17;
+    t->seed ^= (uint64)time(0) << 5;
+    if (!t->seed) t->seed = XOR64_OFFSET;
+    return;
+}
+
+uint64 getXor64(Xor64* t)
+{
+    uint64 target = t->seed;
+    target ^= target >> 12;
+    target ^= target << 25;
+    target ^= target >> 27;
+    t->seed = target;
+    return target * 0x2545F4914F6CDD1DULL;
+}
+
+uint64 getXor64Num(Xor64* t, uint64 min, uint64 max)
+{
+    // min == max == 0 -> no range option
+    return (min == max == 0) ? getXor64(t) : min + getXor64(t) % (max - min);
+}
+
+bool randXor64(uint64 min, uint64 max, const int count, SortOpt op)
+{
+    Xor64 t;
+    initXor64(&t);
+
+    t.result = (uint64*)malloc(sizeof(uint64) * count);
+    t.count = count;
+    for (int i = 0; i < count; i++)
+        t.result[i] = getXor64Num(&t, min, max);
+
+    free(t.result);
+    return true;
+}
